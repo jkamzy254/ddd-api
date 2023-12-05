bcd7d8d9dafa3496dae1d9c45130f96c4221334d
Dilan Horana <dilan.horana@aroflo.com>
2023-12-05 21:34:47 +1100

    Update CORS

--- ddd/ddd/settings.py
+++ ddd/ddd/settings.py
@@ -87,7 +87,8 @@ CORS_ORIGIN_WHITELIST = (
     'https://ang-dj.herokuapp.com',
     'https://ddd-trial1-27b98437c571.herokuapp.com',
     'https://cadillac-dpi-vr-bd6205ea960b.herokuapp.com',
-    'https://ddd84-test.herokuapp.com'
+    'https://ddd84-test.herokuapp.com',
+    'https://ddd-api-test-9dc082dd01e6.herokuapp.com'
 )
 CORS_ALLOW_CREDENTIALS = True
 

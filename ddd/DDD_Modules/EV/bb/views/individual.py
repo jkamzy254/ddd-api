51cf8b3390822081363e67b16162fd4869eda63f
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2024-02-25 19:01:06 +1100

    Commit Master DDD 1: Add getAllSeasons

--- ddd/DDD_Modules/EV/bb/views/individual.py
+++ ddd/DDD_Modules/EV/bb/views/individual.py
@@ -41,3 +41,22 @@ class BBGetBBFruitsViewSet(APIView):
         except Exception as e:
             # Handle exceptions here, e.g., logging or returning an error response
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class BBGetAllSeasonsViewSet(APIView):
+    def get(self, request):
+        
+        try:
+            payload = decode_jwt(request)   
+            # user = Memberdata.objects.filter(id = payload['ID']).first()
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT * FROM EVSeason 
+                    WHERE StartDate < CONVERT(DATE, SYSDATETIMEOFFSET() AT TIME ZONE 'AUS Eastern Standard Time') 
+                    AND ClosingDate > CONVERT(DATE, SYSDATETIMEOFFSET() AT TIME ZONE 'AUS Eastern Standard Time')
+                """)
+                studs = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
+
+            return Response(studs, status=status.HTTP_200_OK)
+        except Exception as e:
+            # Handle exceptions here, e.g., logging or returning an error response
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

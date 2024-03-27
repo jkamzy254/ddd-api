fec446d1bd1c9a42889ef165fa977aa1065b1fea
codey314 <scott.southall10@gmail.com>
2024-03-27 22:20:10 +1100

    remove subdiv parameter from SVCGetAbsentList

--- ddd/DDD_Modules/MT/svc/views/mt_dept.py
+++ ddd/DDD_Modules/MT/svc/views/mt_dept.py
@@ -38,7 +38,7 @@ class SVCGetAbsentList(APIView):
             token = decode_jwt(request)
             payload = request.data
             with connection.cursor() as cursor:
-                cursor.execute(f"SELECT * FROM AbsentList('{request.GET.get('subdiv')}', {request.GET.get('sid')})")
+                cursor.execute(f"SELECT * FROM AbsentList({request.GET.get('sid')})")
                 res = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
             return Response(res, status=status.HTTP_200_OK)
         except Exception as e:

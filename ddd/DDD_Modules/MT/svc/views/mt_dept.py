d0506e58355ee90fc4090a945eb0a066260ec603
codey314 <scott.southall10@gmail.com>
2024-04-18 20:09:36 +1000

    Updating WedSunBreakdown view. Table becomes view. Moved from dept to mt page

--- ddd/DDD_Modules/MT/svc/views/mt_dept.py
+++ ddd/DDD_Modules/MT/svc/views/mt_dept.py
@@ -11,6 +11,20 @@ from django.db import connection
 
 
 
+class SVCGetDeptWedSunBreakdown(APIView):
+    def get(self, request):
+        try:
+            token = decode_jwt(request)
+            with connection.cursor() as cursor:
+                cursor.execute(f"SELECT * FROM Service_DeptWedSunBreakdown")
+                res = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
+            return Response(res, status=status.HTTP_200_OK)
+        except Exception as e:
+            # Handle exceptions here, e.g., logging or returning an error response
+            print(e)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 
 
 class SVCGetAbsenteeByDivision(APIView):

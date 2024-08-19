26a9ad71f6d5f021040390a8dfa765e16de94e95
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2024-08-19 20:42:39 +1000

    Commit AS 5: Restrict Login to users with student in previous CT

--- ddd/apps_script/views/closure_after_fortnight.py
+++ ddd/apps_script/views/closure_after_fortnight.py
@@ -20,7 +20,8 @@ class GetMemberViewSet(APIView):
         
         try:
             with connection.cursor() as cursor:
-                cursor.execute(f"Select UID, PREFERRED_NAME as 'Name' From MemberData Where BBT = 1 And Username = '{username}' And Password = '{password}'")
+                cursor.execute(f"""Select UID, PREFERRED_NAME as 'Name' From MemberData 
+                               Where BBT = 1 And Username = '{username}' And Password = '{password}' AND UID IN (Select BBT_ID From PrevSsnStudentsView)""")
                 result = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
                 
             if len(result) == 0:

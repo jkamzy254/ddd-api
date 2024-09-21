cd80bf7f2e72d398d772d65b0d9f54e46a8f284c
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2024-09-21 12:54:26 +1000

    Commit AS 9: Correct 'GetMemberViewSet' query from 'SELECT * FROM BBDataView' to 'SELECT BBT_ID FROM BBDataView'

--- ddd/apps_script/views/bb_registration.py
+++ ddd/apps_script/views/bb_registration.py
@@ -17,7 +17,7 @@ class GetMemberViewSet(APIView):
             with connection.cursor() as cursor:
                 cursor.execute(f"""Select UID, PREFERRED_NAME as 'Name' From MemberData 
                                Where BBT = 1 And Username = '{username}' And Password = '{password}' AND UID IN (
-                                   SELECT * FROM BBDataView WHERE Season IN (Select ID From EVSeason WHERE ClosingDate > (SELECT SYSDATETIMEOFFSET() AT TIME ZONE 'AUS Eastern Standard Time'))
+                                   SELECT BBT_ID FROM BBDataView WHERE Season IN (Select ID From EVSeason WHERE ClosingDate > (SELECT SYSDATETIMEOFFSET() AT TIME ZONE 'AUS Eastern Standard Time'))
                                )""")
                 result = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
                 

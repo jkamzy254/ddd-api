b72a7e36bae3ff3137c129354a25f7e850062cc5
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2024-12-23 16:13:07 +1100

    Codey AS 19: Add CT Attendance

--- ddd/apps_script/urls.py
+++ ddd/apps_script/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include, re_path
 # from . import views
 from .views import closure_after_fortnight as caf
 from .views import bb_registration as br
+from .views import ct_attendance as ca
 from django.views.generic.base import TemplateView
 
 from django.urls import path
@@ -20,4 +21,12 @@ urlpatterns = [
 	path('bbRegGetStudents/', br.GetStudentsViewSet.as_view()),
 	path('bbRegGetSuburbs/', br.GetSuburbsViewSet.as_view()),
 	path('bbRegReportStudent/', br.ReportStudentViewSet.as_view()),
+ 
+	#CT Attendance
+	path('getStudentList/', ca.CTGetStudentListViewSet.as_view()),
+	path('getWeeklySchedule/', ca.CTGetWeeklyScheduleViewSet.as_view()),
+	path('getAttendance/', ca.CTGetAttendanceViewSet.as_view()),
+	path('updateSchedule/', ca.CTUpdateScheduleViewSet.as_view()),
+	path('updateAttendance/', ca.CTUpdateAttendanceViewSet.as_view()),
+ 
 ]

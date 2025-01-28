3b2aebc6ad577e7bb2b471a716afde1e1f64064f
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2025-01-28 22:42:51 +1100

    Commit AS 30: Update 'ctUpdateSchedule' to only take one record rather than an array

--- ddd/apps_script/views/ct_attendance.py
+++ ddd/apps_script/views/ct_attendance.py
@@ -99,10 +99,9 @@ class CTUpdateScheduleViewSet(APIView):
         data = request.data
         try:
             with connection.cursor() as cursor:
-                for rec in data:
-                    cursor.execute(f"""EXEC spCTUpdateSchedule 
-                        @CTID = {rec.get('CTID')}, @Date = {rec.get('Date')}, @Topic = {rec.get('Topic')}
-                    """)
+                cursor.execute(f"""EXEC spCTUpdateSchedule 
+                    @CTID = {data.get('CTID')}, @Date = {data.get('Date')}, @Topic = {data.get('Topic')}
+                """)
                 result = f"Update for CT Day {data[0]['Date']} done"
 
             return Response(result, status=status.HTTP_200_OK)
@@ -132,6 +131,18 @@ class CTUpdateStudentStatusViewSet(APIView):
                 cursor.execute(f"""EXEC spCTUpdateStudentStatus
                     @UID = '{rec.get('UID')}', @Registration = {rec.get('Registration')}, @Status = {rec.get('Status')}
                 """)
+                result = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()][0]
+
+            return Response(result, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class CTScheduleAddDaysViewSet(APIView):
+    def post(self, request):
+        rec = request.data
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("EXEC spCTScheduleAddClasses")
                 result = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()][0]
 
             return Response(result, status=status.HTTP_200_OK)

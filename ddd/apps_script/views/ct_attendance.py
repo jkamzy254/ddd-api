a65d4f9c1dcc6754584e2c20e89794086c905fab
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2025-04-14 23:17:34 +1000

    Commit AS 34: Add period for class summary

--- ddd/apps_script/views/ct_attendance.py
+++ ddd/apps_script/views/ct_attendance.py
@@ -41,9 +41,10 @@ class GetMemberViewSet(APIView):
 class CTGetAttendanceSummaryViewSet(APIView):
     def get(self, request):
         uid = request.GET.get("UID")
+        period = request.GET.get("Period")
         try:
             with connection.cursor() as cursor:
-                cursor.execute(f"EXEC spCTGetAttendanceSummary {uid}")
+                cursor.execute(f"EXEC spCTGetAttendanceSummary @TGW = '{uid}', @Period = '{period}'")
                 result = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
 
             return Response(result, status=status.HTTP_200_OK)

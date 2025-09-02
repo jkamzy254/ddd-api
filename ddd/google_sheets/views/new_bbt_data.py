c99c3d02f10752374de4ea58ece2551b0b77d87a
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2025-09-03 07:33:15 +1000

    Commit Sheets 7: Fix NBD Functions

--- ddd/google_sheets/views/new_bbt_data.py
+++ ddd/google_sheets/views/new_bbt_data.py
@@ -227,7 +227,7 @@ class GetCurrentCTDataViewSet(APIView):
         season = request.data['season']
         try:
             with connection.cursor() as cursor:
-                cursor.execute(f"SELECT * FROM SheetCurrentCTDataFn({season})")
+                cursor.execute(f"SELECT BBT_Status, BtmNo, Dept, Grp, BBT, pNew, pOld, bbA	stbl, cctA, bbME, cctI, pFA, bbFA, Total FROM SheetCurrentCTDataFn({season})")
                 result = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
                 
             return Response(result, status=status.HTTP_200_OK)

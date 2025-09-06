02a45409ccebc3aaf1c1a43211cc055022ce42db
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2025-09-07 00:13:49 +1000

    Commit DDD 93: Add Pic ID to update

--- ddd/DDD_Modules/EV/fruit_profile/views.py
+++ ddd/DDD_Modules/EV/fruit_profile/views.py
@@ -168,6 +168,7 @@ class FMPUpdateFruitDataViewSet(APIView):
                     @Denomination = '{form['denomination']}',
                     @Notes = '{form['notes']}',
                     @Location = '{form['location']}',
+                    @PicID = '{form['picid']}',
                     @ReporterID = '{token['UID']}'
                 """)
                 fp_rec = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]

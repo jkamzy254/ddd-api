0c73a7a4ed892fea7a9271fc835adefffd18e01d
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2025-10-28 00:03:41 +1100

    Commit AS 64: Fix exam report issue

--- ddd/apps_script/views/tribe_exam.py
+++ ddd/apps_script/views/tribe_exam.py
@@ -73,7 +73,7 @@ class UpdateExamScoreViewSet(APIView):
         try:
             with connection.cursor() as cursor:
                 
-                cursor.execute(f"EXEC spExamReportScore @ExamID = {examid}, @ID = '{uid}', @Score = {score}, @Reason = '{reason}', @Reporter = '{reporter}'")
+                cursor.execute(f"EXEC spExamReportScore @ExamID = {examid}, @UID = '{uid}', @Score = {score}, @Reason = '{reason}', @Reporter = '{reporter}'")
                 result = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
                 
             return Response(result, status=status.HTTP_200_OK)

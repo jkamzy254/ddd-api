e8f63903c3518434074805f31dcb819943423ed6
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2026-02-23 23:26:16 +1100

    Commit DDD 86.1: Add Edu filter for Edu module

--- ddd/DDD_Modules/Edu/attendance/views/dept.py
+++ ddd/DDD_Modules/Edu/attendance/views/dept.py
@@ -29,7 +29,7 @@ class EDUGetWeekEducations(APIView):
         try:
             token = decode_jwt(request)
             with connection.cursor() as cursor:
-                cursor.execute(f"SELECT * FROM Education_GetEducations")
+                cursor.execute("SELECT * FROM Education_GetEducations Where Status IS NOT NULL Or Type = 'Drop-In'")
                 res = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
             return Response(res, status=status.HTTP_200_OK)
         except Exception as e:

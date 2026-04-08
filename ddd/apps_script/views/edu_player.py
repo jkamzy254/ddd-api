2edb44ce243033b12b921fb65ca21955da25a037
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2026-04-08 22:34:07 +1000

    Commit AS 84.3: Add filter for Active HSP files

--- ddd/apps_script/views/edu_player.py
+++ ddd/apps_script/views/edu_player.py
@@ -53,7 +53,7 @@ class HSPGetMaterialViewSet(APIView):
         uid = request.GET.get("UID")
         try:
             with connection.cursor() as cursor:
-                cursor.execute(f"SELECT * FROM NewEduMaterialTable")
+                cursor.execute(f"SELECT * FROM NewEduMaterialTable WHERE Active = 1")
                 res = [dict(zip([column[0] for column in cursor.description], record)) for record in cursor.fetchall()]
             return Response(res, status=status.HTTP_200_OK)
         except Exception as e:
